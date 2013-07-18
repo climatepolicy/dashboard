c27e3ef37cbddff5a502cd60ea79e4d30477eb0a
Andrew Hobbs <andrew@Andrew-Hobbss-MacBook-Pro.local>
2013-07-17 23:48:12 -0600

    shrunk the bargraph height

--- bargraph.js
+++ bargraph.js
@@ -1,7 +1,7 @@
 function bargraph(csvfile, sectornames, where){
 
 var w = document.getElementById(where).offsetWidth,
-    h = w*(2/3),
+    h = w*(1/2),
 	des_space = 100,
     p = [20, 50, 130, 20],
     x = d3.scale.ordinal().rangeRoundBands([50, w]),
