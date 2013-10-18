01c1d3a6047d7db8a50270ae991b05cbed77948e
awhobbs <andrew@cpisf.org>
2013-10-18 19:44:51 +0200

    I think I fixed the dumb bar graphs

--- js/bargraph.js
+++ js/bargraph.js
@@ -3,7 +3,7 @@ function bargraph(csvfile, sectortext, vertspots, where){
 var w = document.getElementById(where).offsetWidth,
     h = w*(3/4),
 	des_space = 100,
-    p = [100, 50, 20, 20],
+    p = [150, 50, 20, 20],
     right_space = 170,
     x = d3.scale.ordinal().rangeRoundBands([50, w - right_space]),
     y = d3.scale.linear().range([0, h - p[0] - p[2]]),
@@ -29,38 +29,24 @@ var svg1 = d3.select("#" + where).append("svg:svg")
 					x: d.date,
 					y: +d[cause],
 					info: i.date,
-					policy: d.policy,
 					cap: d.cap
 				};
 			});
 		}));
-		
 
 		function mouseOver(){
-			var thisSector = d3.select(this);
-			thisSector.selectAll("rect").style("fill-opacity", "1");
-			groupname.html(function(d, i){
-					return thisSector.attr("desc");
-				});
-		}
-		
-		function mouseOut(){
-			var thisSector = d3.select(this);
-			thisSector.selectAll("rect").style("fill-opacity", "0.8");
-			groupname.html(sectortext[0]);
-		}
-		
-		/*function click(){
-			rect.attr("transform", function(d){
-				return "translate(" + x(d.x1) + "," + (-y(d.y0) - y(d.y)) + ")";
-			});
-			policytext.attr("visibility", "hidden");
-			var newlabel = svg.selectAll("horses").data(x.domain()).enter().append("svg:text").attr("x", function(d){
-				return x(d) + x.rangeBand() / 2;
-			}).attr("y", 6).attr("text-anchor", "middle").attr("dy", ".71em").text(function(d, i){
-				return d;
-			})
-		}*/
+            var thisSector = d3.select(this);
+            thisSector.selectAll("rect").style("fill-opacity", "1");
+            thisSector.selectAll(".group-label").attr("visibility", "visible");
+            groupname.html(thisSector.attr("desc"));
+        }
+        
+        function mouseOut(){
+            var thisSector = d3.select(this);
+            thisSector.selectAll("rect").style("fill-opacity", "0.8");
+            thisSector.selectAll(".group-label").attr("visibility", "hidden");
+            groupname.html(sectortext[0]);
+        }
 		
 		// Compute the x-domain (by date) and y-domain (by top).
 		x.domain(sectors[0].map(function(d){
@@ -81,7 +67,7 @@ var svg1 = d3.select("#" + where).append("svg:svg")
 			.style("fill", function(d, i){
 				return z(i);
 			})
-			.on("mouseover", mouseOver).on("mouseout", mouseOut);//.on("click", click);
+			.on("mouseover", mouseOver).on("mouseout", mouseOut);
 			
 		 var lineFunction = d3.svg.line()
 			.x(function(d) { return x(d.x); })
@@ -95,6 +81,17 @@ var svg1 = d3.select("#" + where).append("svg:svg")
 			.attr("stroke-width", 2)
 			.attr("fill", "none");
 			
+		var groupname = svg1
+                .append('foreignObject')
+                .attr("x", 0)
+                .attr("y",20)
+                .attr("transform", "translate(0 " + -h + p[2] + ")")
+                .attr('width', w)
+                .attr('height', p[0])
+                .append("xhtml:div")
+                .style("background-color","white")
+                .html(sectortext[0]);
+			
 	   //label sectors
 	   sector
 	       .append("svg:text")
@@ -104,7 +101,6 @@ var svg1 = d3.select("#" + where).append("svg:svg")
 	        .attr("class", "subtext")
 	        .attr("fill", "#666")
 	       .text(function(d,i){return sectornames[i];});
-	
 		
 		// Add a rect for each date.
 		var rect = sector.selectAll("rect").data(Object).enter().append("svg:rect").attr("transform", function(d){
@@ -117,17 +113,6 @@ var svg1 = d3.select("#" + where).append("svg:svg")
 				
 			}).attr("width", x.rangeBand())
            .style("fill-opacity", "0.8");
-		
-		var dots = sector.selectAll("circle")
-		 .data(Object)
-		 .enter().append("svg:circle")
-		 .attr("class", "group-label")
-		 .attr("visibility", "hidden")
-		 .attr("fill", "black")
-		 .attr("cx", function(d) { return x(d.x) + x.rangeBand()/2; })
-		 .attr("cy", function(d) { return -y(d.y0) - y(d.y) -10; })
-		 .attr("r", function(d,i){if(d.policy==='1' && d.y>0 && d.y<300){return 5;} else{return 0;}});
-		
 		
 		// Add a label per date.
 		var label = svg1.selectAll("g.labels").data(x.domain()).enter().append("svg:text").attr("x", function(d){
@@ -144,38 +129,15 @@ var svg1 = d3.select("#" + where).append("svg:svg")
 			}
 		});
 		
-		//add group info	  
 		
-			/*d3.select('#describe').append("svg:svg")
-    			.attr("width", w)
-    			.attr("height", h)
-  				.append("svg:g")
-    			.attr("transform", "translate(" + p[3] + "," + (h - p[2]) + ")")
-				.select('#describe')
-				.append("svg:svg")
-    			.attr("width", 50)
-    			.attr("height", 50)*/
-		
-		var groupname = svg1
-				.append('foreignObject')
-				.attr("class", "group-label")
-				.attr("x", 0)
-				.attr("y", -h+p[2])
-				.attr('width', w)
-                .attr('height', 100)
-				.append("xhtml:p")
-				.style("background-color","white")
-				.html(sectortext[0]);
-		
-		d3.selectAll("p").style("stroke","red");
-		
+				
 		// Add y-axis rules.
 		var rule = svg1.selectAll("g.rule").data(y.ticks(5)).enter().append("svg:g").attr("class", "rule").attr("transform", function(d){
 			return "translate(20," + -y(d) + ")";
 		});
 		
 		rule.attr("class", "subtext").append("svg:text").attr("text-anchor", "end").attr("x", p[1]).attr("dx", -25).attr("dy", ".35em").text(d3.format(",d"));
-		
+		/*
 		// add y-axis label
 		svg1.append("foreignObject")
       		.attr("transform", "rotate(-90)")
@@ -186,9 +148,9 @@ var svg1 = d3.select("#" + where).append("svg:svg")
 			.attr("class", "subtext")
 			.append("xhtml")
       		.style("text-anchor", "end")
-      		.html('Annual Emissions (MMTCO<sub>2</sub>e)');
+      		.html('Annual Emissions (MMTCO<sub>2</sub>e)');*/
 			
-				keydata = [{name:'CAPPED', color: capcolor}, {name:'UNCAPPED', color: uncapcolor}];
+		keydata = [{name:'CAPPED', color: capcolor}, {name:'UNCAPPED', color: uncapcolor}];
 		
 		key = svg1.append('svg:g');
 		key.selectAll("keyrect")
